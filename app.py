2bc68989ded326743c10a3999ef61db3c32d8260
Michael Miller <mnm364@gmail.com>
2018-02-16 15:22:43 -0500

    removed file upload stuff and moduled out registration logic

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from db_helpers import readdb, writedb
 from flask import Flask, jsonify, request, render_template, json, flash, redirect, Session, url_for
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired, FileAllowed
-from werkzeug.utils import secure_filename
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from register import RegistrationForm, register_user
 
 app = Flask(__name__)
 app.secret_key = "SECRET-KEY"
@@ -11,37 +8,15 @@ app.secret_key = "SECRET-KEY"
 SUCCESS = json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
 
-class RegistrationForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
-    password = PasswordField('New Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match')
-    ])
-    confirm = PasswordField('Repeat Password')
-    name = StringField('Name')
-    sex = StringField('Sex')
-    relationship = StringField('Relationship Status')
-    school = StringField('School')
-    concentration = StringField('Concentration')
-    status = StringField('Status')
-    picture_filename = StringField('Profile Photo Filename')
-    # picture = FileField(validators=[
-    #     FileRequired(),
-    #     FileAllowed(['jpg', 'png'], 'Images only!')])
-
 @app.route('/')
 def index():
     return render_template('login.html')
-
-@app.route('/db')
-def db(): pass
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm(request.form)
     if request.method == 'POST' and form.validate():
-        _create_user(dict(
+        register_user(dict(
             username=form.username.data,
             password=form.password.data,
             email=form.email.data,
@@ -65,7 +40,6 @@ def display_user(username):
 
     friends = readdb('friends').get(username, [])
     user['friends'] = friends
-    # return jsonify(user)
     return render_template('profile.html', user=user)
 
 @app.errorhandler(404)
@@ -76,19 +50,9 @@ def page_not_found(e):
 def create_user(username):
     user = request.get_json()
     user['username'] = username
-    _create_user(user)
+    register_user(user)
 
     return SUCCESS
-
-def _create_user(user):
-    # if 'picture' in user:
-    #     picture.save(os.path.join(
-    #         'db', 'profilepics', username))
-    #     user['picture'] = user['picture'].name
-
-    content = readdb('users')
-    content[user['username']] = user
-    writedb('users', content)
 
 @app.route('/createfriend', methods=['POST'])
 def create_friend():
---
+++ register.py
@@ -0,0 +1,25 @@
+from db_helpers import readdb, writedb
+from flask_wtf import FlaskForm
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+
+
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    name = StringField('Name')
+    sex = StringField('Sex')
+    relationship = StringField('Relationship Status')
+    school = StringField('School')
+    concentration = StringField('Concentration')
+    status = StringField('Status')
+    picture_filename = StringField('Profile Photo Filename')
+
+def register_user(user):
+    content = readdb('users')
+    content[user['username']] = user
+    writedb('users', content)
