6213202c031ce796d9c99538d8d49b2451523459
Michael Miller <mnm364@gmail.com>
2018-02-17 01:27:59 -0500

    tutorial: link friends list to profiles

--- server.py
+++ server.py
@@ -1,19 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from utils.db import readdb, writedb
 
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
-    # TODO - render hello world template
-    return 'Hello World'
+    return render_template('hello_child.html', value='Hello World')
 
 @app.route('/user/<username>', methods=['GET'])
 def user(username):
     user = readdb('users').get(username)
     friends = readdb('friends').get(username, [])
     user['friends'] = friends
-    return jsonify(user)
+    return render_template('profile.html', user=user)
 
 if __name__ == '__main__':
     app.debug = True
