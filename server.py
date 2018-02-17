b8e14102c4c6f7dcf3c70012333a13b37012fe39
Michael Miller <mnm364@gmail.com>
2018-02-17 01:10:21 -0500

    tutorial: add friends to user response

--- server.py
+++ server.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+from utils.db import readdb, writedb
 
 app = Flask(__name__)
 
-# TODO - render "Hello World" at extension /hello
+@app.route('/hello')
+def hello():
+    return 'Hello World'
+
+@app.route('/user/<username>', methods=['GET'])
+def user(username):
+    user = readdb('users').get(username)
+
+    # TODO - add friends to user response
+
+    return jsonify(user)
 
 if __name__ == '__main__':
     app.debug = True
